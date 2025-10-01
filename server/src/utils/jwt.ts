22f5f8390fcb47437bedaa47e622e0c903325400
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-10-01 23:01:07 +0200

    🚨 Refactor JWT handling to improve type safety

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -4,10 +4,19 @@ import { Request, Response, NextFunction } from "express";
 const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret";
 const JWT_EXPIRES_IN = 3600000; // Token expiration time in ms
 
-interface JwtPayload {
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace Express {
+        interface Request {
+            userId?: string;
+        }
+    }
+}
+
+export type JwtPayload = {
     userId: string;
     expiresAt: number;
-}
+};
 
 // Function to generate a JWT for a given user ID
 export function generateToken(userId: string): string {
@@ -19,19 +28,24 @@ export function generateToken(userId: string): string {
 }
 
 // Middleware to verify JWT and attach user info to the request
-export function verifyToken(req: Request, res: Response, next: NextFunction) {
+export async function verifyToken(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
     const authHeader = req.headers.authorization;
     if (!authHeader)
         return res.status(401).send({ message: "No token provided" });
     const token = authHeader.split(" ")[1]; // Bearer <token>
     if (!token) return res.status(401).send({ message: "No token provided" });
-    jwt.verify(token, JWT_SECRET, (err: any, decoded: any) => {
-        if (err || !decoded || typeof decoded === "string")
-            return res.status(401).send({ message: "Invalid token" });
-        if ((decoded as JwtPayload).expiresAt < Date.now())
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const { userId, expiresAt } = decoded as JwtPayload;
+        if (expiresAt < Date.now())
             return res.status(401).send({ message: "Token has expired" });
-        // Attach userId to request object for further use
-        (req as any).userId = (decoded as JwtPayload).userId;
+        req.userId = userId;
         next();
-    });
+    } catch (error) {
+        return res.status(401).send({ message: "Invalid token" });
+    }
 }
