3c288a1b7adbc6ae4b6ca2ec0a14fb919809270e
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2026-01-16 17:17:08 +0100

    ✨ Add message model and socket action for sending messages in boards

---
+++ server/src/routes/socket_actions/chat.send.ts
@@ -0,0 +1,25 @@
+import { SocketAction } from "./action_type";
+import prisma from "../../utils/prisma.client";
+
+export const messageAction: SocketAction = {
+    actionName: "chat.send",
+    async execute(boardId: string, data: string, userId: string) {
+        console.info(`Message "${data}" sent in board ${boardId} by user ${userId}`);
+        const message = await prisma.message.create({
+            data: {
+                boardId: boardId,
+                userId: userId,
+                content: data,
+            }
+        });
+        if (!message) {
+            console.error("Failed to create message");
+            throw new Error("Failed to create message");
+        }
+        console.info(`Message created with ID: ${message.id}`);
+        return {
+            createdAt: message.createdAt,
+            content: message.content
+        };
+    }
+};
