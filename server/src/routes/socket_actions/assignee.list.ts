a840a97de28ebf84caacba004ad23a458fc6e571
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2026-01-16 17:45:52 +0100

    🔒️ Fix security issue Fix a situation that sent the password hash when fetching assignees

--- server/src/routes/socket_actions/assignee.list.ts
+++ server/src/routes/socket_actions/assignee.list.ts
@@ -17,7 +17,14 @@ export const assigneeListingAction: SocketAction = {
         );
         const assignees = await prisma.user.findMany({
             where: { assigned_cards: { some: { id: listData.cardId } } },
-            orderBy: { username: "asc" }
+            orderBy: { username: "asc" },
+            select: {
+                id: true,
+                username: true,
+                email: true,
+                createdAt: true,
+                updatedAt: true
+            }
         });
         console.info(`Assignees found: ${assignees.length}`);
         return { cardId: listData.cardId, assignees };
